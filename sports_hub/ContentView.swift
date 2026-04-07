0bfb9b4563d45852f683bc70422ee46348babef5
lukehkelly <lhkelly@unc.edu>
2026-04-07 16:02:30 -0400

    Initial Commit

---
+++ sports_hub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sports_hub
+//
+//  Created by Luke Kelly on 4/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sports_hub/sports_hubApp.swift
@@ -0,0 +1,17 @@
+//
+//  sports_hubApp.swift
+//  sports_hub
+//
+//  Created by Luke Kelly on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct sports_hubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
